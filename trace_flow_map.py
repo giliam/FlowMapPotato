ad0ed1377f74fedd098b73b075cbc47b9f1a09d8
giliam <matthieu.denoux@gmail.com>
2020-11-02 14:45:33 +0100

    Nicer English version.

--- trace_flow_map.py
+++ trace_flow_map.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 import math
+import logging
 
 # libraries
 import geopandas as gpd
@@ -10,24 +11,29 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-# Paramètres
-# Taille de l'image
+# Enables logging
+logging.basicConfig(level=logging.INFO)
+
+# Parameters
+# Picture size
 figsize = (10, 10)
-# Résolution de l'image
+# Image resolution
 dpi = 200
-# Taille des ronds bleus
+# Size of countries circles
 markersize = 500
-# Taille de police des noms des pays
+# Country color
+country_bubblecolor = "#4897CA"
+# Country name font size
 fontsize = 12
-# Paramètre de décalage des flèches
+# Arrow offset from circle
 factormarker = 150
-# Colormap des flèches
+# Arrows color map
 colormaparrows = mpl.cm.get_cmap("RdYlGn_r", 25)
-# Courbure des flèches
+# Arrows radius of curvature
 radius_arrow = 0.05
-# Activer ronds non concernés
+# Enable countries non used
 enable_non_used_country_names = False
-# Paramètres caractéristiques des flèches
+# Additional arrows parameters
 param_arrows = "head_width=2, head_length=3"
 arrowswidth = 2
 # Angle offset
@@ -39,38 +45,36 @@ INPUT_FILE = "input_data"
 OUTPUT_FILENAME = "test"
 
 
-# Cartes
-pays_europeens = gpd.read_file("cartes/pays.shp", encoding="utf-8")
-centroids = gpd.read_file("cartes/pays_centroids.shp", encoding="utf-8")
+# Maps
+logging.info("Imports maps")
+pays_europeens = gpd.read_file("maps/pays.shp", encoding="utf-8")
+centroids = gpd.read_file("maps/pays_centroids.shp", encoding="utf-8")
 
-# Données des échanges
+# Exchange data
+logging.info("Imports input data in data/%s.csv", INPUT_FILE)
 df = pd.read_csv(f"data/{INPUT_FILE}.csv")
 df.replace(to_replace=countries_id_to_replace, inplace=True)
 
-# On calcule l'étendue de valeurs
-data_extent = df["Value"].max() - df["Value"].min()
-
-# On récupère juste les coordonnées des centroids (préalablement calculés)
+# We get centroids coordinates
 centroids["coords"] = centroids["geometry"].apply(lambda x: x.coords[:])
 centroids["coords"] = [coords[0] for coords in centroids["coords"]]
 
 
-def plot_fond_de_carte(ax):
+def plot_countries_layer(ax):
     """
-    Affiche le fond de carte avec les contours des pays européens
+    Display the countries layer
     """
     pays_europeens.plot(color="white", linewidth=0.5, edgecolor="gray", ax=ax, zorder=1)
 
 
-def plot_noms_pays(ax):
+def plot_countries_names(ax):
     """
-    Affiche les noms des pays dans une bulle bleue. Calcule également les localisations
-    des-dits pays.
+    Display the countries IDs in colored bubble.
 
-    :return: dictionnaire des coordonnées selon l'ID (2 lettres) des pays
+    :return: countries bubbles coordinates dict by ID (2 letters)
     :rtype: dict
     """
-    # Pour chaque centroid on affiche un gros rond bleu
+    # For each centroid, we add a bubble if used in flows
     if not enable_non_used_country_names:
         filtered_centroids = centroids.loc[
             (centroids["CNTR_ID"].isin(df["Export"]))
@@ -78,13 +82,12 @@ def plot_noms_pays(ax):
         ]
     else:
         filtered_centroids = centroids
-
     filtered_centroids.plot(
-        ax=ax, marker="o", color="#4897CA", markersize=markersize, zorder=4
+        ax=ax, marker="o", color=country_bubblecolor, markersize=markersize, zorder=4
     )
 
     countries_locations = {}
-    # On affiche ensuite les initiales du pays sur chaque rond
+    # We then display ID on each bubble
     for idx, row in filtered_centroids.iterrows():
         row["coords"] = (row["coords"][0], row["coords"][1])
         plt.text(
@@ -101,47 +104,54 @@ def plot_noms_pays(ax):
     return countries_locations
 
 
-def plot_map(filename="test"):
+def plot_map(filename="test", title=None):
     """
-    Dessine la carte avec les flèches
+    Draws flow map
 
-    :param filename: nom du fichier de sortie, dans dossier output, defaults to "test"
+    :param filename: output file name in folder outputs (default: test)
     :type filename: str, optional
     """
     plt.figure(figsize=figsize, dpi=dpi)
 
-    # On crée la grille avec les ratios pour la colorbar de droite
+    # We create the grid for map and colorbar
     gs = gridspec.GridSpec(3, 2, width_ratios=[75, 1], height_ratios=[1, 3, 1])
 
-    # Emplacement de la carte
+    # Removes axis and frame
     ax = plt.subplot(gs[:, 0], frameon=False)
     x_axis = ax.axes.get_xaxis()
     x_axis.set_visible(False)
     y_axis = ax.axes.get_yaxis()
     y_axis.set_visible(False)
 
-    # Fond de carte
-    plot_fond_de_carte(ax)
+    # Countries boundaries
+    logging.info("Plots countries")
+    plot_countries_layer(ax)
 
-    # Ronds pays
-    countries_locations = plot_noms_pays(ax)
+    # Bubbles
+    logging.info("Plots names")
+    countries_locations = plot_countries_names(ax)
 
-    # Il faut maintenant calculer les flèches
+    # We compute data extent of flows (for colorbar)
+    data_extent = df["Value"].max() - df["Value"].min()
+
+    # Arrows
+    logging.info("Computes arrows")
     already_drawn = []
     for idx, row in df.iterrows():
         coords_export = countries_locations[row["Export"]]
         coords_import = countries_locations[row["Import"]]
 
-        # On crée un identifiant unique pour la paire export/import
-        # pour éviter de tracer la flèche deux fois au même endroit
+        # Unique ID for export/import couple to avoid plotting the two sides
+        # arrows at the same spot
         joint_name = "".join(sorted([row["Export"], row["Import"]]))
-        # Si la flèche a déjà été tracée, on l'inverse
-        if joint_name in already_drawn:
-            inverse = -1.0
-        else:
-            inverse = 1.0
 
-        # On récupère le point à gauche et celui à droite
+        # If the couple has already been involved, we have to revert the arrow
+        if joint_name in already_drawn:
+            revert_arrow = -1.0
+        else:
+            revert_arrow = 1.0
+
+        # We get left and right dot
         left_dot = (
             coords_export if coords_export[0] < coords_import[0] else coords_import
         )
@@ -150,26 +160,26 @@ def plot_map(filename="test"):
         )
         inverted_left_right = "->" if coords_export[0] < coords_import[0] else "<-"
 
-        # On détermine lequel est le plus haut (change la valeur des signes de décalage)
+        # We determine which one is higher
         if left_dot[1] > right_dot[1]:
             left_upper = 1.0
         else:
             left_upper = -1.0
 
-        # On calcule l'angle entre les deux points
+        # We compute angle between both points
         alpha = math.atan((left_dot[1] - right_dot[1]) / (left_dot[0] - right_dot[0]))
 
-        # Si on a inversé la flèche, on la trace "au-dessus" du segment reliant les deux points
-        # c'est-à-dire on ajoute un petit angle de décalage positif
-        # Sinon, on trace "en-dessous" du segment
-        if inverse > 0:
+        # If we reverted the arrow, we plot it "above" the line between the points
+        # i.e. with a small angle offset
+        # otherwise, we plot it "below"
+        if revert_arrow > 0:
             betaleft = alpha - left_upper * angle_offset
             betaright = alpha + left_upper * angle_offset
         else:
             betaleft = alpha + left_upper * angle_offset
             betaright = alpha - left_upper * angle_offset
 
-        # On calcule les nouvelles coordonnées des points
+        # We compute the new coordinates
         new_left_dot = (
             left_dot[0] + markersize * factormarker * math.cos(betaleft),
             left_dot[1] + markersize * factormarker * math.sin(betaleft),
@@ -179,12 +189,12 @@ def plot_map(filename="test"):
             right_dot[1] - markersize * factormarker * math.sin(betaright),
         )
 
-        # On crée la flèche
+        # On create the arrow
         arrow = patches.FancyArrowPatch(
             new_left_dot,
             new_right_dot,
             connectionstyle=patches.ConnectionStyle.Arc3(
-                rad=inverse * left_upper * radius_arrow
+                rad=revert_arrow * left_upper * radius_arrow
             ),
             color=colormaparrows(row["Value"] / data_extent),
             linewidth=arrowswidth,
@@ -193,24 +203,28 @@ def plot_map(filename="test"):
         )
         plt.gca().add_patch(arrow)
 
-        # On ajoute la paire Import/Export aux flèches déjà tracées
+        # We add the couple to already drawn couples
         already_drawn.append(joint_name)
 
-    # On ajoute la colorbar à droite
+    # Colorbar
+    logging.info("Adds colorbar")
     ax2 = plt.subplot(gs[1, 1])
-    # On utilise le min - max des valeurs pour la tracer
+    # We use max - min values for normalization
     norm = mpl.colors.Normalize(vmin=df["Value"].min(), vmax=df["Value"].max())
 
     cb1 = mpl.colorbar.ColorbarBase(
         ax2, cmap=colormaparrows, norm=norm, orientation="vertical"
     )
 
-    # On enregistre le fichier
+    # We add suptitle
+    plt.suptitle(title)
+    # Saving file
+    logging.info("Saves output!")
     plt.tight_layout()
     plt.savefig(f"output/{filename}.png", dpi=dpi, bbox_inches="tight")
     plt.close()
 
-    print("Plots succeeded...")
+    logging.info("Plots succeeded!")
 
 
 plot_map(OUTPUT_FILENAME)
